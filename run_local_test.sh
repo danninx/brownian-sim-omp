cf99f4b79344fb3518c4ac71a1582de1ac304533
Daniel Fajardo <mail@dfajardo.dev>
2026-03-28 09:24:28 -0500

    update with larger test sizes

--- run_local_test.sh
+++ run_local_test.sh
@@ -11,16 +11,16 @@ echo ""
 echo "Running with 10,000 particles, D=1.0, time_step=0.001 seconds, simulated_time=100.000 seconds"
 echo ""
 echo "Running serial..."
-./bin/brownian_serial 10000 1.0 0.01 100.0 > serial.csv
+./bin/brownian_serial 10000 1.0 0.001 100.0 > serial.csv
 echo ""
 echo "Running single-process multithreaded..."
-./bin/brownian_omp 10000 1.0 0.01 100.0 > multithreaded.csv
+./bin/brownian_omp 10000 1.0 0.001 100.0 > multithreaded.csv
 echo ""
 echo "Running single threaded MPI..."
-./bin/brownian_mpi 10000 1.0 0.01 100.0 > mpi.csv
+./bin/brownian_mpi 10000 1.0 0.001 100.0 > mpi.csv
 echo ""
 echo "Running multithreaded MPI..."
-./bin/brownian_mpi_omp 10000 1.0 0.01 100.0 > mpi_omp.csv
+./bin/brownian_mpi_omp 10000 1.0 0.001 100.0 > mpi_omp.csv
 
 echo "Generating MSD vs. time graphs..."
 python results.py serial.csv
