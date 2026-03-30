9d6a26132541110371a7ce2c94cbef50c1db5dba
Daniel Fajardo <mail@dfajardo.dev>
2026-03-29 20:42:04 -0500

    add some helpful comments

--- src/rng.c
+++ src/rng.c
@@ -56,25 +56,31 @@ static int global_max_threads = 1;
 static thread_local unsigned int thread_seed;
 static thread_local int seed_initialized = 0;
 
+// setup thread-independent seed
 void rng_init_thread(int tid) {
 	thread_seed = (unsigned int) (global_base_seed + (global_rank * global_max_threads) + tid);
 	seed_initialized = 1;
 }
 
+// setup the global RNG settings for this process
 void rng_init_custom(unsigned long base_seed, int max_threads, int rank) {
 	global_max_threads = max_threads;
 	global_base_seed = base_seed;
 	global_rank = rank;
 }
 
+// U ~ [0, 1]
 double uniform_random(int tid) {
 	if (!seed_initialized) {
 		rng_init_thread(tid);
 	}
 
+	// use rand_r() with the thread seed over rand() for thread safety (see `man rand_r`)
 	return (double) rand_r(&thread_seed) / RAND_MAX;
 }
 
+// thread safe Box-Muller with \sigma = sigma
+// see https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform#Basic_form
 double rng_gaussian_custom(int tid, double sigma) {
 	double u1 = uniform_random(tid);
 	double u2 = uniform_random(tid);
@@ -87,12 +93,19 @@ double rng_gaussian_custom(int tid, double sigma) {
 	return z0 * sigma;
 }
 
+// thread local and static global variables will be cleaned up on program exit, unlike the gsl_rng's which are malloc'd
 void rng_cleanup_custom(int max_threads) {
 	return;
 }
 
 #endif
 
+//
+// rng interface defines the three main functionalities we need for the simulation (at the moment anyhow)
+// both the implementation and source codes are preprocessed based on whether GSL is compiled with the code
+// by default it will use the custom implementations, but if you want to try it with GSL use the `make with_gsl`
+// Makefile target
+//
 void rng_init(unsigned long base_seed, int max_threads, int rank) {
 	#ifdef USE_GSL
 		rng_init_gsl(base_seed, max_threads, rank);
--- src/rng.h
+++ src/rng.h
@@ -6,8 +6,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+//
+// rng interface (see source for more details)
+//
+
+// initialize rng parameters for this process
 void rng_init(unsigned long base_seed, int max_threads, int rank);
+
+// gaussian RNG using thread_id (for seeding) and sigma
 double rng_gaussian(int tid, double sigma);
+
+// cleanup rng memory (if needed)
 void rng_cleanup(int max_threads);
 
 #endif
